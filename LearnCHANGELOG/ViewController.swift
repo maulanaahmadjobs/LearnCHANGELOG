d7aaaef1415d8eace057cd2a6bed9767a8cee27c
Maulana West <maulanaahmadjobs@gmail.com>
2025-11-30 23:15:56 +0700

    Initial Commit

---
+++ LearnCHANGELOG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LearnCHANGELOG
+//
+//  Created by west on 30/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
