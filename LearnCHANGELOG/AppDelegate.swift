45aa44278fedc4f9069bfdad09ad5d3bc47bfbc0
Maulana West <maulanaahmadjobs@gmail.com>
2025-12-02 02:06:16 +0700

    fix: move comment di delegate

--- LearnCHANGELOG/AppDelegate.swift
+++ LearnCHANGELOG/AppDelegate.swift
@@ -10,11 +10,10 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    // TODO: Add welcome screen
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        // TODO: Add welcome screen
         return true
     }
 
